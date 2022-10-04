d5c09f492876cfe677f553b1fab59e3c0f59eb49
coderhop <user.name>
2022-10-04 15:23:23 -0400

    Updated with Jococo

---
+++ src/test/java/com/coderhop/rebound/exception/ReboundExceptionHandlerTest.java
@@ -0,0 +1,14 @@
+package com.coderhop.rebound.exception;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ReboundExceptionHandlerTest {
+
+	@Test
+	void test() {
+		
+	}
+
+}
